2c92ccfa3e3ad38ec8559e2b62ba676c3b937002
pmikel01 <pmikel01@ucy.ac.cy>
2020-11-19 14:13:50 +0200

    Main App changes

--- src/sample/RegisterController.java
+++ src/sample/RegisterController.java
@@ -5,9 +5,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class RegisterController {
     ObservableList<String> genderList = FXCollections.observableArrayList("Male", "Female", "Other");
@@ -19,6 +23,7 @@ public class RegisterController {
     public void pressExitButton(ActionEvent event) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+        openStage("sign_in.fxml");
     }
 
     @FXML
@@ -28,15 +33,43 @@ public class RegisterController {
     public void pressSignInButtonButton(ActionEvent event) {
         Stage stage = (Stage) signInButton.getScene().getWindow();
         stage.close();
+        openStage("sign_in.fxml");
     }
 
     @FXML
-    private ComboBox genderBox;
+    private ComboBox<String> genderBox;
 
     @FXML
     private void initialize() {
         genderBox.setValue("Male");
         genderBox.setItems(genderList);
+    }
+
+    private double x, y;
+
+    private void openStage(String fileName) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fileName));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+
+            stage.setScene(new Scene(root1));
+            stage.initStyle(StageStyle.UNDECORATED);
+
+            //we gonna drag the frame
+            root1.setOnMousePressed(mouseEvent -> {
+                x = mouseEvent.getSceneX();
+                y = mouseEvent.getSceneY();
+            });
+
+            root1.setOnMouseDragged(mouseEvent -> {
+                stage.setX(mouseEvent.getScreenX() - x);
+                stage.setY(mouseEvent.getScreenY() - y);
+            });
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load window");
+        }
     }
 
 }
--- src/sample/SignInController.java
+++ src/sample/SignInController.java
@@ -27,31 +27,10 @@ public class SignInController {
 
     @FXML
     public void handleRegisterButton(ActionEvent event) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-
-            stage.setScene(new Scene(root1));
-            stage.initStyle(StageStyle.UNDECORATED);
-
-            //we gonna drag the frame
-            root1.setOnMousePressed(mouseEvent -> {
-                x = mouseEvent.getSceneX();
-                y = mouseEvent.getSceneY();
-            });
-
-            root1.setOnMouseDragged(mouseEvent -> {
-                stage.setX(mouseEvent.getScreenX() - x);
-                stage.setY(mouseEvent.getScreenY() - y);
-            });
-            stage.show();
-        } catch (Exception e) {
-            System.out.println("Cant load window");
-        }
+        Stage stage = (Stage) registerButton.getScene().getWindow();
+        stage.close();
+        openStage("register.fxml");
     }
-
-    private double x1, y1;
 
     @FXML
     private Button signInButton;
@@ -60,12 +39,12 @@ public class SignInController {
     public void handleSignInButton(ActionEvent event) {
         Stage stage = (Stage) signInButton.getScene().getWindow();
         stage.close();
-        openStage();
+        openStage("main_app.fxml");
     }
 
-    public void openStage() {
+    private void openStage(String fileName) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("app_choices.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fileName));
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
 
