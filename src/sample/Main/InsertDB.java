8718575b365ca6aa54392438a5894dd951f21850
pmikel01 <pmikel01@ucy.ac.cy>
2020-11-30 16:05:46 +0200

    Insert

--- src/sample/Main/InsertDB.java
+++ src/sample/Main/InsertDB.java
@@ -10,17 +10,42 @@ import java.util.Scanner;
 public class InsertDB {
     private static final String SQL_INSERT_LOC = "INSERT INTO [dbo].LOCATION (Name) VALUES (?)";
     private static final String SQL_INSERT_USER = "INSERT INTO [dbo].PROFILE (Username,Password,FirstName,LastName,Birthday,Email,Website,Gender,Verified,Birthplace,LivesIn,Link) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+    private static final String SQL_INSERT_FRIENDS = "INSERT INTO [dbo].FRIENDS (USER_ID,FRIEND_ID) VALUES (?,?)";
     private static final String SQL_INSERT_INTEREST = "INSERT INTO [dbo].INTERESTS (Name) VALUES (?)";
     private static final String SQL_INSERT_USER_INTEREST = "INSERT INTO [dbo].PROFILES_INTERESTS (INTERESTS_ID,USER_ID) VALUES (?,?)";
-    private static final String SQL_INSERT_FRIENDS = "INSERT INTO [dbo].FRIENDS (USER_ID,FRIEND_ID) VALUES (?,?)";
 
     public static void insertData(Connection conn){
-//        insertLocations(conn);
+        insertLocations(conn);
         insertUsers(conn);
-//        insertInterests(conn);
-//        insertFriends(conn);
-//        insertUserInterests(conn);
+        insertFriends(conn);
+        insertInterests(conn);
+        insertUserInterests(conn);
     }
+
+    private static void insertLocations(Connection conn){
+        File file = new File("cities.txt");
+        Scanner read;
+        try {
+            read = new Scanner(file);
+            read.nextLine();
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                String[] data = line.split("\t");
+                PreparedStatement preparedStatement;
+                try {
+                    preparedStatement = conn.prepareStatement(SQL_INSERT_LOC);
+                    preparedStatement.setString(1, data[1]);
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.err.println("error in statement location");
+                }
+            }
+        } catch (FileNotFoundException e1) {
+            System.err.println("File not found");
+        }
+    }
+
     private static void insertUsers(Connection conn){
         int i = 1;
         File file = new File("users.txt");
@@ -54,7 +79,9 @@ public class InsertDB {
                     preparedStatement.setString(6, email);
                     String web = "www.socialFace.com/username"+i;
                     preparedStatement.setString(7, web);
-                    preparedStatement.setBoolean(8,Boolean.parseBoolean(data[3]));
+                    Boolean gender = null;
+                    gender = data[3].equals("1");
+                    preparedStatement.setBoolean(8,gender);
                     preparedStatement.setBoolean(9,false);
                     preparedStatement.setInt(10, Integer.parseInt(data[4]));
                     preparedStatement.setInt(11, Integer.parseInt(data[5]));
@@ -63,16 +90,16 @@ public class InsertDB {
                     i++;
                 } catch (SQLException e) {
                     e.printStackTrace();
-                    System.err.println("Prepared Statement error Users .");
+                    System.err.println("error in statement users");
                 }
             }
         } catch (FileNotFoundException e1) {
-            System.err.println("File for user not found");
+            System.err.println("File not found");
         }
     }
 
-    private static void insertLocations(Connection conn){
-        File file = new File("cities.txt");
+    private static void insertFriends(Connection conn){
+        File file = new File("friends.txt");
         Scanner read;
         try {
             read = new Scanner(file);
@@ -82,27 +109,69 @@ public class InsertDB {
                 String[] data = line.split("\t");
                 PreparedStatement preparedStatement;
                 try {
-                    preparedStatement = conn.prepareStatement(SQL_INSERT_LOC);
-                    preparedStatement.setString(1, data[1]);
-                    preparedStatement.executeUpdate();
+                    if (!data[0].equals(data[1])) {
+                        preparedStatement = conn.prepareStatement(SQL_INSERT_FRIENDS);
+                        preparedStatement.setInt(1, Integer.parseInt(data[0]));
+                        preparedStatement.setInt(2, Integer.parseInt(data[1]));
+                        preparedStatement.executeUpdate();
+                    }
                 } catch (SQLException e) {
                     e.printStackTrace();
-                    System.err.println("Prepared Statement error Location .");
+                    System.err.println("error in statement friends");
                 }
             }
         } catch (FileNotFoundException e1) {
-            System.err.println("File for locations not found");
+            System.err.println("File not found");
         }
     }
 
     private static void insertInterests(Connection conn){
-
+        File file = new File("interests.txt");
+        Scanner read;
+        try {
+            read = new Scanner(file);
+            read.nextLine();
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                String[] data = line.split("\t");
+                PreparedStatement preparedStatement;
+                try {
+                    preparedStatement = conn.prepareStatement(SQL_INSERT_INTEREST);
+                    preparedStatement.setString(1, data[1]);
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.err.println("error in statement interests");
+                }
+            }
+        } catch (FileNotFoundException e1) {
+            System.err.println("File not found");
+        }
     }
-    private static void insertFriends(Connection conn){
 
-    }
     private static void insertUserInterests(Connection conn){
-
+        File file = new File("user_interests.txt");
+        Scanner read;
+        try {
+            read = new Scanner(file);
+            read.nextLine();
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                String[] data = line.split("\t");
+                PreparedStatement preparedStatement;
+                try {
+                    preparedStatement = conn.prepareStatement(SQL_INSERT_USER_INTEREST);
+                    preparedStatement.setInt(1, Integer.parseInt(data[1]));
+                    preparedStatement.setInt(2, Integer.parseInt(data[0]));
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.err.println("error in statement user interests");
+                }
+            }
+        } catch (FileNotFoundException e1) {
+            System.err.println("File not found");
+        }
     }
 
 }
